605c97461ce794b86caab91d28e36f2a6a50ac35
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-07 11:34:21 -0300

    add cabecalho.php and remove head tag ing galeria.php and cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <title>Clorocine</title>
-</head>
+<?php include_once "cabecalho.php"?>
 
 <body>
     <nav class="nav-extended purple lighten-3">
